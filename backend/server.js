e9920463336e593ffd696cb5ac0ce2461f18d38b
neghi 14 <sambasseynengimote@gmail.com>
2023-01-05 23:12:46 +0100

    Third push, Configured for vercel

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = require('./index');
 
+dotenv.config('.env');
+
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/IdeaApp').then(() => {
+mongoose.connect(db).then(() => {
   console.log('DB CONNECTED SUCCESSFULLY');
 });
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`APP STARTED ON ${PORT}`);
